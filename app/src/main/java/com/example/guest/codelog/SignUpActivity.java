5a1688f04bf78aa90df21ef68f858a2cbee73b8f
rh <ryanjharvey88@gmail.com>
2016-07-20 09:28:51 -0700

    Added required views to sign up activity xml

--- app/src/main/java/com/example/guest/codelog/SignUpActivity.java
+++ app/src/main/java/com/example/guest/codelog/SignUpActivity.java
@@ -3,11 +3,15 @@ package com.example.guest.codelog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
     }
 }
