5e574c4e670a65dd10f770368931c27fdf2ffd68
rh <ryanjharvey88@gmail.com>
2016-07-20 16:35:10 -0700

    Created custom list adapter for project list

---
+++ app/src/main/java/com/example/guest/codelog/adapters/ProjectListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.guest.codelog.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.guest.codelog.R;
+import com.example.guest.codelog.models.Project;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Guest on 7/20/16.
+ */
+public class ProjectListAdapter extends RecyclerView.Adapter<ProjectListAdapter.ProjectViewHolder>  {
+    private ArrayList<Project> mProjects = new ArrayList<>();
+    private Context mContext;
+
+    public ProjectListAdapter(Context context, ArrayList<Project> projects){
+        mContext = context;
+        mProjects = projects;
+
+    }
+    @Override
+    public ProjectListAdapter.ProjectViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_list_item, parent, false);
+        ProjectViewHolder viewHolder = new ProjectViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ProjectListAdapter.ProjectViewHolder holder, int position) {
+        holder.bindProject(mProjects.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProjects.size();
+    }
+
+    public class ProjectViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.projectListItemTitle) TextView mProjectListItemTitle;
+
+        private Context mContext;
+
+        public ProjectViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindProject (Project project){
+            mProjectListItemTitle.setText(project.getProjectName());
+        }
+    }
+}
