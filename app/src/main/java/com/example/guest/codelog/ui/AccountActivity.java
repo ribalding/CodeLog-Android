06615713b8174681e421b16a2eac09ad2ac4a3f0
rh <ryanjharvey88@gmail.com>
2016-07-20 11:41:55 -0700

    Added New Project Activity

--- app/src/main/java/com/example/guest/codelog/ui/AccountActivity.java
+++ app/src/main/java/com/example/guest/codelog/ui/AccountActivity.java
@@ -1,15 +1,33 @@
 package com.example.guest.codelog.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.guest.codelog.R;
 
-public class AccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.newProject) Button mNewProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        ButterKnife.bind(this);
+
+        mNewProject.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mNewProject){
+            Intent newProjectIntent = new Intent(AccountActivity.this, NewProjectActivity.class);
+        }
     }
 }
---
+++ app/src/main/java/com/example/guest/codelog/ui/NewProjectActivity.java
@@ -0,0 +1,15 @@
+package com.example.guest.codelog.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guest.codelog.R;
+
+public class NewProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project);
+    }
+}
