cc779f583aee05c2baeb8d6b631c644c6619a381
rh <ryanjharvey88@gmail.com>
2016-07-20 11:10:42 -0700

    Log In Button on Main Activity will now log a user in and send them to the Account Activity

--- app/src/main/java/com/example/guest/codelog/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/codelog/ui/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.guest.codelog.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.guest.codelog.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
@@ -37,12 +42,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view == mLogInButton){
-            Intent loginIntent = new Intent(MainActivity.this, AccountActivity.class);
-            startActivity(loginIntent);
+            login();
         } else if (view == mSignUpButton){
             Intent signUpIntent = new Intent(MainActivity.this, SignUpActivity.class);
             startActivity(signUpIntent);
         }
+    }
+
+    private void login(){
+
+        final String email = mEnterEmail.getText().toString();
+        final String pass = mEnterPassword.getText().toString();
+
+
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent loginIntent = new Intent (MainActivity.this, AccountActivity.class);
+                            startActivity(loginIntent);
+                            finish();
+                        } else if (!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Please Try Again", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
     }
 
 }
